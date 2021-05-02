be86dcf6f3a7b271e2fffa78fe43ce5e9afe69cb
Akash1012 <gupta.akash2090@gmail.com>
2021-05-02 09:47:20 +0530

    add redux-form package

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
+import { reducer } from 'redux-form'
 
 import authReducer from './authReducer'
 
 const rootReducer = combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    form: reducer
 })
 
 export default rootReducer
