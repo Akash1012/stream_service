376c094b643c6e8bf5d6cf182e82da507560ce14
Akash1012 <gupta.akash2090@gmail.com>
2021-05-02 12:08:22 +0530

    Add validation

--- src/compoments/streamCreate.js
+++ src/compoments/streamCreate.js
@@ -2,26 +2,39 @@ import React, { useState } from 'react';
 import { Field, reduxForm } from 'redux-form'
 
 const StreamCreate = (props) => {
-    console.log(props)
+
+    const renderError = (meta) => {
+        console.log(meta.touched)
+        console.log(meta.error)
+        if (meta.touched && meta.error) {
+            return (
+                <div className="ui error message">
+                    <div className="header">{meta.error}</div>
+                </div>
+            )
+        }
+    }
 
     const renderInput = (formProps) => {
+        console.log("formProps", formProps)
+        const className = `{field ${formProps.meta.error && formProps.meta.touched ? 'error' : ''}}`
         return (
-            <div className="field">
+            <div className={className}>
                 <label>{formProps.label}</label>
-                <input onChange={formProps.input.onChange} value={formProps.input.value} />
-
+                <input autoComplete="off" {...formProps.input} />
+                {/* onChange={formProps.input.onChange} value={formProps.input.value} */}
+                {renderError(formProps.meta)}
             </div>
         )
     }
 
     const onSubmit = (formValues) => {
-
-
+        console.log(formValues)
     }
 
     return (
         <div>
-            <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+            <form className="ui form error" onSubmit={props.handleSubmit(onSubmit)}>
                 <Field label="Enter Title" name="title" component={renderInput} />
                 <Field label="Enter Description" name="description" component={renderInput} />
                 <button className="ui button primary">Submit</button>
@@ -44,5 +57,6 @@ const validate = (formValues) => {
 }
 
 export default reduxForm({
-    form: 'streamCreate'
+    form: 'streamCreate',
+    validate: validate
 })(StreamCreate)
