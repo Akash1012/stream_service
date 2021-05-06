45a162037e59224e217133b1368730a6baacea99
Akash1012 <gupta.akash2090@gmail.com>
2021-05-06 18:48:05 +0530

    remove bubble problem and add stop propagation

--- src/compoments/Modal.js
+++ src/compoments/Modal.js
@@ -8,7 +8,7 @@ import history from '../actions/history'
 const Modal = (props) => {
     return ReactDOM.createPortal(
         <div onClick={() => history.push('/')} className="ui dimmer modals visible active">
-            <div className="ui standard modal visible active">
+            <div onClick={(e) => e.stopPropagation()} className="ui standard modal visible active">
                 <div className="header"> Delete Stream</div>
                 <div className="content">Are you want to delete this stream</div>
                 <div className="actions">
